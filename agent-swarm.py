6de7e361de0bac2431b03cd73531d0ba2c50b13c
Agam Kushwaha <56078504+agamkus@users.noreply.github.com>
2025-02-17 12:50:38 +0530

    Add files via upload

---
+++ agent-swarm.py
@@ -0,0 +1,94 @@
+from swarm import Agent
+from swarm.repl import run_demo_loop
+
+ 
+
+
+def execute_order(product, price: int):
+    """Price should be in USD."""
+    print("\n\n=== Order Summary ===")
+    print(f"Product: {product}")
+    print(f"Price: ${price}")
+    print("=================\n")
+    confirm = input("Confirm order? y/n: ").strip().lower()
+    if confirm == "y":
+        print("Order execution successful!")
+        return "Success"
+    else:
+        print("Order cancelled!")
+        return "User cancelled order."
+
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="Determine which agent is best suited to handle the user's request, and transfer the conversation to that agent.",
+)
+sales_agent = Agent(
+    name="Sales Agent",
+    instructions=(
+        "You are a sales agent for ACME Inc."
+        "Always answer in a sentence or less."
+        "Follow the following routine with the user:"
+        "1. Ask them about any problems in their life related to product they are looking for.\n"
+        "2. Casually mention one of ACME's crazy made-up products can help.\n"
+        " - Don't mention price.\n"
+        "3. Once the user is bought in, drop a ridiculous price.\n"
+        "4. Only after everything, and if the user says yes, "
+        "tell them a crazy caveat and execute their order.\n"
+        ""),
+    functions=[execute_order],
+)
+
+def look_up_item(search_query):
+    """Use to find item ID.
+    Search query can be a description or keywords."""
+    item_id = "item_132612938"
+    print("Found item:", item_id)
+    return item_id
+
+
+def execute_refund(item_id, reason="not provided"):
+    print("\n\n=== Refund Summary ===")
+    print(f"Item ID: {item_id}")
+    print(f"Reason: {reason}")
+    print("=================\n")
+    #print("Refund execution successful!")
+    return "success"
+
+
+issues_and_repairs_agent = Agent(
+    name="Issues and Repairs Agent",
+    instructions=(
+        "You are a customer support agent for ACME Inc."
+        "Always answer in a sentence or less."
+        "Follow the following routine with the user:"
+        "1. First, ask probing questions and understand the user's problem deeper.\n"
+        " - unless the user has already provided a reason.\n"
+        "2. Propose a fix (make one up).\n"
+        "3. ONLY if not satesfied, offer a refund.\n"
+        "4. If accepted, search for the ID and then execute refund."
+        ""
+    ),
+    functions=[execute_refund, look_up_item],
+)
+
+def transfer_back_to_triage():
+    """Call this function if a user is asking about a topic that is not handled by the current agent."""
+    return triage_agent
+
+def transfer_to_sales():
+    return sales_agent
+
+def transfer_to_refunds():
+    return issues_and_repairs_agent
+
+def transfer_to_sales_after_refund():
+    """Once a refund is processed, transfer the user to the Sales Agent to assist with purchasing a new product."""
+    print("[mock] Transferring user to Sales Agent after refund...")
+    return sales_agent
+
+triage_agent.functions = [transfer_to_sales, transfer_to_refunds]
+sales_agent.functions.append(transfer_back_to_triage)
+issues_and_repairs_agent.functions.append(transfer_to_sales_after_refund)  
+
+if __name__ == "__main__":
+    run_demo_loop(triage_agent)
